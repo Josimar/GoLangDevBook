228f5d7977563043d67ffc968fca7a9aa6ae1770
Josimar Silva <josimar.silva@me.com.br>
2021-09-03 16:10:21 -0300

    Controller executando as rotas

--- api/src/router/router.go
+++ api/src/router/router.go
@@ -1,8 +1,13 @@
 package router
 
-import "github.com/gorilla/mux"
+import (
+	"api/src/router/rotas"
+	"github.com/gorilla/mux"
+)
 
 // Gerar -> retorna uma router com suas rotas configuradas
 func Gerar() *mux.Router {
-	return mux.NewRouter()
+	r := mux.NewRouter()
+
+	return rotas.Configurar(r)
 }
