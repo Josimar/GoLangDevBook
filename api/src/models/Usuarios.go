5ce85a439f6b670b1e3930704a8ed40860d4cd67
Josimar Silva <josimar.silva@me.com.br>
2021-09-03 16:23:16 -0300

    Model usuários

---
+++ api/src/models/Usuarios.go
@@ -0,0 +1,12 @@
+package models
+
+import "time"
+
+// Usuario -> estrutura da tabela usuário
+type Usuario struct {
+	ID        uint64    `json:"id,omitempty"`
+	Name      string    `json:"name,omitempty"`
+	Email     string    `json:"email,omitempty"`
+	Password  string    `json:"password,omitempty"`
+	CreatedAt time.Time `json:"created_at,omitempty"`
+}
